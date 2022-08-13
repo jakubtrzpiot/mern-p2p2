bcad7b15924f2b97f760d5df9a9a727bc7065305
jakubtrzpiot <j.trzpiot03@gmail.com>
2022-08-13 15:20:43 +0200

    restore some of the funcionalities basing on useState()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,85 +5,75 @@ import { Button, Input } from './components/CustomComponents';
 
 export const App = () => {
   const [username, setUsername] = useState('');
-  const [avatar, setAvatar] = useState('');
+  const [avatar, setAvatar] = useState(null);
   let navigate = useNavigate();
 
   // REWRITE TO FIT OUR NODEJS BACKEND //
 
-  /* onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setUsername(user.displayName);
-      setAvatar(user.photoURL);
-    } else {
-      navigate('/login', { replace: true });
-    } 
-  });
-  const onSignOut = () => {
-    signOut(auth)
-      .then(() => {})
-      .catch((error) => {
-        alert(error.code);
-      });
-  };
-  const user = auth.currentUser;
+  // onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     setUsername(user.displayName);
+  //     setAvatar(user.photoURL);
+  //   } else {
+  //     navigate('/login', { replace: true });
+  //   }
+  // });
+  // const onSignOut = () => {
+  //   signOut(auth)
+  //     .then(() => {})
+  //     .catch((error) => {
+  //       alert(error.code);
+  //     });
+  // };
+  // const user = auth.currentUser;
   const onUpdateProfile = (e) => {
     e.preventDefault();
-    let obj = {};
-    let { username, avatar } = e.target;
-    username.value
-      ? (obj.displayName = username.value)
-      : (obj.displayName = user.displayName);
-    avatar.value
-      ? (obj.photoURL = avatar.value)
-      : (obj.photoURL = user.photoURL);
-    updateProfile(user, obj).then(() => {
-      // Update successful.
-      setUsername(user.displayName);
-      setAvatar(user.photoURL);
-      e.target.reset(); //Clear inputs
-    });
-  };
-  */
-  //check if current password matches then update user password
-  const onUpdatePassword = (e) => {
-    e.preventDefault();
-    let { password, newPassword, reNewPassword } = e.target;
-    console.log(newPassword.value);
-    /* if (newPassword.value === reNewPassword.value && newPassword !== '') {
-      const credential = EmailAuthProvider.credential(
-        auth.currentUser.email,
-        password.value
-      );
-      reauthenticateWithCredential(user, credential)
-        .then(() => {
-          // User re-authenticated.
-          console.log(e.target);
-          updatePassword(user, newPassword.value)
-            .then(() => {
-              alert('Password changed.');
-              navigate('/login', { replace: true });
-            })
-            .catch((error) => {
-              // An error ocurred
-              alert(error.code);
-            });
-        })
-        .catch((error) => {
-          // An error ocurred
-          alert(error.code);
-        });
-    } else {
-      alert('Password do not match');
-    }
+    let { newUsername, newAvatar } = e.target;
+    if (newUsername.value) setUsername(newUsername.value);
+    if (newAvatar.value) setAvatar(newAvatar.value);
     e.target.reset();
+    // updateProfile(user, obj).then(() => {
+    //   // Update successful.
+    // });
   };
+
+  //check if current password matches then update user password
+  // const onUpdatePassword = (e) => {
+  //   e.preventDefault();
+  //   let { password, newPassword, reNewPassword } = e.target;
+  // if (newPassword.value === reNewPassword.value && newPassword !== '') {
+  //     const credential = EmailAuthProvider.credential(
+  //       auth.currentUser.email,
+  //       password.value
+  //     );
+  //     reauthenticateWithCredential(user, credential)
+  //       .then(() => {
+  //         // User re-authenticated.
+  //         console.log(e.target);
+  //         updatePassword(user, newPassword.value)
+  //           .then(() => {
+  //             alert('Password changed.');
+  //             navigate('/login', { replace: true });
+  //           })
+  //           .catch((error) => {
+  //             // An error ocurred
+  //             alert(error.code);
+  //           });
+  //       })
+  //       .catch((error) => {
+  //         // An error ocurred
+  //         alert(error.code);
+  //       });
+  //   } else {
+  //     alert('Password do not match');
+  //   }
+  //   e.target.reset();
+  // };
 
   //delete the user avatar
   const onDeleteAvatar = () => {
-    updateProfile(user, { photoURL: '' }).then(() => {
-      setAvatar(user.photoURL);
-    });
-    */
+    setAvatar(null);
+    // updateProfile(user, { photoURL: '' }).then(() => {});
   };
 
   return (
@@ -104,22 +94,22 @@ export const App = () => {
       <div className="col-start-2 row-start-2 flex justify-center">
         <form
           className="w-[280px] flex flex-col justify-center"
-          //onSubmit={onUpdateProfile}
+          onSubmit={onUpdateProfile}
         >
-          <Input name="username" content="Enter new name" />
-          <Input name="avatar" content="Enter new avatar URL" />
+          <Input name="newUsername" content="Enter new name" />
+          <Input name="newAvatar" content="Enter new avatar URL" />
           <Button content="Update Profile" />
         </form>
         <div className="flex flex-col justify-center items-center">
           <Button
             className="ml-2 w-40"
-            //func={onDeleteAvatar}
+            func={onDeleteAvatar}
             content="Delete Avatar"
           />
         </div>
       </div>
       <form
-        onSubmit={onUpdatePassword}
+        //onSubmit={onUpdatePassword}
         className="w-[280px] col-start-2 row-start-3 flex flex-col justify-center justify-self-center"
       >
         <Input name="password" isPassword content="Enter your password" />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Figtree', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#D9A744',
+      },
+    },
   },
   plugins: [],
-}
+};
