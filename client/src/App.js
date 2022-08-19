320fb0a9d5db1e805742c3bde1488e5c698b67fa
Jakub Trzpiot <j.trzpiot03@gmail.com>
2022-08-19 02:20:47 +0200

    add homepage

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { Admin } from './pages/Admin';
 import { Dashboard } from './pages/Dashboard';
+import { Home } from './pages/Home';
 
 export const App = () => {
   return (
     <div className="bg-[#222222] h-screen flex">
       <Routes>
+        <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/dashboard" element={<Dashboard />}></Route>
         <Route exact path="/login" element={<Login />}></Route>
         <Route exact path="/signup" element={<Signup />}></Route>
