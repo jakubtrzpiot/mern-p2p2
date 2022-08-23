b5d0d1251a193295728e31010b5f5cd17b98b23c
voozen <voozen3@gmail.com>
2022-08-23 16:52:07 +0200

    change home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { isAuth } from '../api/UserApi';
+import { Button } from '../components/CustomComponents';
+import { SignupForm } from '../components/SignupForm';
 
 export const Home = props => {
   const navigate = useNavigate();
@@ -19,24 +21,13 @@ export const Home = props => {
   }
 
   return (
-    <div className="bg-homeBackground bg-cover w-full h-screen bg-center">
+    <div className="bg-homeBackground bg-cover w-full bg-center">
       <div className='max-w-xl mx-auto'>
-        <h1 className='text-white text-9xl font-bold text-center mt-20 mb-10 lg:text-6xl'>
+        <h1 className='text-white text-6xl font-bold text-center mt-20 mb-10 lg:text-8xl'>
           <p>Connect</p> <p>with</p> <p>anyone</p> <p>anywhere</p>
         </h1>
       </div>
-      <div className='max-w-xl mx-auto flex items-center flex-col gap-5 text-6xl lg:text-3xl text-black'>
-        <Button
-          className='py-6 px-12 font-bold hover:bg-orange-400'
-          content='Sign up'
-          func={toSignup}
-        />
-        <Button
-          className='py-6 px-12 font-bold hover:bg-orange-400'
-          content='Log in'
-          func={toLogin}
-        />
-      </div>
+      <SignupForm/>
     </div>
   );
 };
