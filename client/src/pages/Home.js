de2d9f821a7d8f6941e78ba906d9c3102c458727
Jakub Trzpiot <j.trzpiot03@gmail.com>
2022-08-23 17:04:12 +0200

    fix homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,24 +7,17 @@ import { BrowserView, MobileView } from 'react-device-detect';
 
 export const Home = props => {
   const navigate = useNavigate();
-  useEffect(() => {
-    isAuth().then(({ currentUser }) =>
-      currentUser
-        ? navigate('/u/' + currentUser.id, { replace: true })
-        : localStorage.visited
-        ? navigate('/login', { replace: true })
-        : (localStorage.visited = true),
-    );
-  }, [navigate]);
+  // useEffect(() => {
+  //   isAuth().then(({ currentUser }) =>
+  //     currentUser
+  //       ? navigate('/u/' + currentUser.id, { replace: true })
+  //       : localStorage.visited
+  //       ? navigate('/login', { replace: true })
+  //       : (localStorage.visited = true),
+  //   );
+  // }, [navigate]);
 
   return (
-    <div className="bg-homeBackground bg-cover w-full bg-center">
-      <div className='max-w-xl mx-auto'>
-        <h1 className='text-white text-6xl font-bold text-center mt-20 mb-10 lg:text-8xl'>
-          <p>Connect</p> <p>with</p> <p>anyone</p> <p>anywhere</p>
-        </h1>
-      </div>
-      <SignupForm/>
     <div className="bg-cover w-full bg-center h-full lg:bg-homeBrowser">
       <h1 className="text-white text-6xl font-bold text-center">
         <p>Connect with</p> <p>anyone,</p> <p>anywhere</p>
@@ -39,7 +32,6 @@ export const Home = props => {
           <SignupForm />
         </BrowserView>
       </div>
-    </div>
     </div>
   );
 };
